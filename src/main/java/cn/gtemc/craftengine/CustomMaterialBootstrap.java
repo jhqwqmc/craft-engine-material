bd6d0d6fb9cfd6e8d5b01c96024d9806233639a4
jhqwqmc <jhcraft@qq.com>
2025-09-19 22:58:00 +0800

    修复低版本启动

--- src/main/java/cn/gtemc/craftengine/CustomMaterialBootstrap.java
+++ src/main/java/cn/gtemc/craftengine/CustomMaterialBootstrap.java
@@ -17,36 +17,10 @@ import java.util.List;
 public class CustomMaterialBootstrap implements PluginBootstrap {
     private static boolean isInitialized = false;
 
-    @SuppressWarnings({"unchecked", "deprecation", "all"})
     @Override
     public void bootstrap(@NotNull BootstrapContext context) {
         if (!isDatapackDiscoveryAvailable()) return;
-        context.getLifecycleManager().registerEventHandler(LifecycleEvents.DATAPACK_DISCOVERY, e -> {
-            if (isInitialized) return;
-            isInitialized = true;
-            context.getLogger().info("The Material enumeration is being dynamically added...");
-            try {
-                List<Material> added = new ArrayList<>();
-                for (Object block : (Iterable<Object>) MBuiltInRegistries.BLOCK) {
-                    Material material = MaterialHelper.createBlockMaterial(block);
-                    if (material != null) added.add(material);
-                }
-                List<Material> materials = new ArrayList<>(Arrays.stream((Material[]) MaterialHelper.field$Material$VALUES.get(null)).toList());
-                materials.addAll(added);
-                ReflectionUtils.UNSAFE.putObjectVolatile(Material.class, ReflectionUtils.UNSAFE.staticFieldOffset(MaterialHelper.field$Material$VALUES), materials.toArray(new Material[0]));
-                MaterialHelper.field$Class$enumConstantDirectory.set(Material.class, null);
-                MaterialHelper.field$Class$enumConstants.set(Material.class, null);
-            } catch (Throwable ex) {
-                context.getLogger().warn("""
-                        Failed to add the Material enumeration dynamically.
-                        You may be able to fix this problem by adding the following command-line argument \
-                        directly after the 'java' command in your start script:\s
-                        '--add-opens java.base/java.lang=ALL-UNNAMED'
-                        """, ex);
-                return;
-            }
-            context.getLogger().info("The dynamic addition of the Material enumeration is complete.");
-        });
+        new INIT(context).init();
     }
 
     private static boolean isDatapackDiscoveryAvailable() {
@@ -58,4 +32,37 @@ public class CustomMaterialBootstrap implements PluginBootstrap {
             return false;
         }
     }
+
+    private record INIT(BootstrapContext context) {
+
+        @SuppressWarnings({"unchecked", "deprecation", "all"})
+        private void init() {
+                context.getLifecycleManager().registerEventHandler(LifecycleEvents.DATAPACK_DISCOVERY, e -> {
+                    if (isInitialized) return;
+                    isInitialized = true;
+                    context.getLogger().info("The Material enumeration is being dynamically added...");
+                    try {
+                        List<Material> added = new ArrayList<>();
+                        for (Object block : (Iterable<Object>) MBuiltInRegistries.BLOCK) {
+                            Material material = MaterialHelper.createBlockMaterial(block);
+                            if (material != null) added.add(material);
+                        }
+                        List<Material> materials = new ArrayList<>(Arrays.stream((Material[]) MaterialHelper.field$Material$VALUES.get(null)).toList());
+                        materials.addAll(added);
+                        ReflectionUtils.UNSAFE.putObjectVolatile(Material.class, ReflectionUtils.UNSAFE.staticFieldOffset(MaterialHelper.field$Material$VALUES), materials.toArray(new Material[0]));
+                        MaterialHelper.field$Class$enumConstantDirectory.set(Material.class, null);
+                        MaterialHelper.field$Class$enumConstants.set(Material.class, null);
+                    } catch (Throwable ex) {
+                        context.getLogger().warn("""
+                                Failed to add the Material enumeration dynamically.
+                                You may be able to fix this problem by adding the following command-line argument \
+                                directly after the 'java' command in your start script:\s
+                                '--add-opens java.base/java.lang=ALL-UNNAMED'
+                                """, ex);
+                        return;
+                    }
+                    context.getLogger().info("The dynamic addition of the Material enumeration is complete.");
+                });
+            }
+        }
 }
